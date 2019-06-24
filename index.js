27fbb715f9ae7e33419854e6eb5671ff7bf8739c
Mike <mikeramzdesign@gmail.com>
2019-06-23 17:51:28 -0700

    react simulator working and creating first component

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-/**
- * @format
- */
+// /**
+//  * @format
+//  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+
+
+// import a library to hel create a component
+
+import React from 'react'
+import ReactNative from 'react-native'
+
+
+
+
+// create a component
+
+const App = () => {
+    return (
+        <Text>Some Text</Text>
+    )
+}
+
+// render it to the device
+//note: for every react app we make we musts register one component
+
+ReactNative.AppRegistry.registerComponent('albums', () => App)
